2d2fde852d07908eafe31608862f4e8dba843ed0
jinvi <jinvi@qq.com>
2018-09-11 17:33:26 +0800

    u

---
+++ sw.js
@@ -0,0 +1,47 @@
+importScripts("/wb-assets/precache-manifest.87606b0e4448bedb5862ad0f8553613b.js", "/wb-assets/workbox-v3.4.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/wb-assets/workbox-v3.4.1"});
+const pjPrefix = 'Cnode-demo'
+
+workbox.core.setCacheNameDetails({prefix: pjPrefix});  //自定义预缓存名
+
+workbox.skipWaiting();  //更新SW跳过等待状态
+workbox.clientsClaim();  //激活后接管已打开的由旧SW控制的页面
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);  //预缓存方法
+
+/*
+* Runtime caching
+* */
+
+//导航网址返回预缓存文件index.html
+workbox.routing.registerNavigationRoute("/");
+
+//缓存字体图标和用户头像
+workbox.routing.registerRoute(
+    new RegExp('(?:http:\\/\\/at\\.alicdn\\.com\\/.*\\.woff)'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: pjPrefix + '-opaque',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.Plugin({
+                maxEntries: 5,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+)
+
+workbox.routing.registerRoute(
+    /img\/.*\.(?:jpg|png|gif)/,
+    workbox.strategies.cacheFirst({
+        cacheName: pjPrefix + '-img',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 5
+            })
+        ]
+    })
+)
+
