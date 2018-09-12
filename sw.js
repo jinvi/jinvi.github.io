81af7afa23ee59af9f53bab31eec7d70be1374ef
jinvi <jinvi@qq.com>
2018-09-12 17:41:52 +0800

    u

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-importScripts("/wb-assets/precache-manifest.87606b0e4448bedb5862ad0f8553613b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
-
+importScripts("/wb-assets/precache-manifest.87606b0e4448bedb5862ad0f8553613b.js", "/wb-assets/workbox-v3.4.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/wb-assets/workbox-v3.4.1"});
 const pjPrefix = 'Cnode-demo'
 
 workbox.core.setCacheNameDetails({prefix: pjPrefix});  //自定义预缓存名
