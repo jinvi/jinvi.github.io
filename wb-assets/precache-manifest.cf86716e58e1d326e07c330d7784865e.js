354f9568ae00760c0b31b80ad870d4c1279541ff
jinvi <jinvi@qq.com>
2018-09-19 17:06:00 +0800

    u

---
+++ wb-assets/precache-manifest.cf86716e58e1d326e07c330d7784865e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a52560153a26e8d4ccd8",
+    "url": "/js/main.js"
+  },
+  {
+    "revision": "e0889c706fdab79c3e2b",
+    "url": "/js/5.js"
+  },
+  {
+    "revision": "0d45caf126d34fa89a7a",
+    "url": "/js/4.js"
+  },
+  {
+    "revision": "b96dd393226d586f5fe1",
+    "url": "/js/3.js"
+  },
+  {
+    "revision": "1a90a7f43adc97ded638",
+    "url": "/js/2.js"
+  },
+  {
+    "revision": "ae668de44fa04f972526",
+    "url": "/js/1.js"
+  },
+  {
+    "revision": "fc338bdd365d377af9cf",
+    "url": "/js/0.js"
+  },
+  {
+    "revision": "d9ff766ba88f5ce40ddd0de14c55fd47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a52560153a26e8d4ccd8",
+    "url": "/css/style.css"
+  }
+];
